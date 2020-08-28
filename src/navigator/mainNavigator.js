bdc30644a037d9ee2c82b0cf00a63b45f9ca8f34
Crowdbotics <team@crowdbotics.com>
2020-08-28 14:04:47 +0000

    add package blueprint-react-native-sign-in-2-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -5,6 +5,7 @@ import {createDrawerNavigator} from 'react-navigation-drawer';
 import SplashScreen from "../features/SplashScreen";
 import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
+import SignIn295114Navigator from '../features/SignIn295114/navigator';
 
 /**
  * new navigators can be imported here
@@ -13,6 +14,7 @@ import SideMenu from './sideMenu';
 const AppNavigator = {
 
     //@BlueprintNavigationInsertion
+SignIn295114: { screen: SignIn295114Navigator },
 
     /** new navigators can be added here */
     SplashScreen: {
